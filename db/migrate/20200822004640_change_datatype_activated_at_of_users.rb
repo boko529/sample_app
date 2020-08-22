9bf4b35d1d5a9e9010c2505ef7edaf2885e0586e
Ubuntu <ubuntu@ip-172-31-14-251.us-east-2.compute.internal>
2020-08-22 06:40:34 +0000

    Fix error2

--- db/migrate/20200822004640_change_datatype_activated_at_of_users.rb
+++ db/migrate/20200822004640_change_datatype_activated_at_of_users.rb
@@ -1,5 +1,7 @@
 class ChangeDatatypeActivatedAtOfUsers < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :activated_at, :datetime
+    add_column :users, :activation_digest, :string
+    add_column :users, :activated, :boolean, default: false
+    add_column :users, :activated_at, :datetime
   end
 end
