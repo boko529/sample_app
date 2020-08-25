b3016754e9e1aef7c145cabe779d0678d8370b58
Ubuntu <ubuntu@ip-172-31-14-251.us-east-2.compute.internal>
2020-08-25 10:27:51 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -67,7 +68,11 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
   
-end
+  #試作feedの定義
+  #完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
     
@@ -81,3 +86,4 @@ end
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+end
